645d1ef127aff4e0d1ca4460b79f5c566740abba
prof-sanjay <sanjayinmarch11@gmail.com>
2025-09-29 12:20:46 +0530

    Updated

--- src/NaturalNumbers.jsx
+++ src/NaturalNumbers.jsx
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import "./index.css";
 
 function NaturalNumbers() {
-  const [num, setNum] = useState("");   // input number
-  const [numbers, setNumbers] = useState([]); // list of natural numbers
+  const [num, setNum] = useState("");
+  const [numbers, setNumbers] = useState([]);
 
-  // Function to generate natural numbers up to num
+
   function nat() {
     const arr = [];
     for (let i = 1; i <= num; i++) {
@@ -18,16 +18,8 @@ function NaturalNumbers() {
     <div className="box">
       <h3>Natural Numbers</h3>
       <label htmlFor="nmb">Number: </label>
-      <input
-        id="nmb"
-        type="number"
-        value={num}
-        onChange={(e) => setNum(e.target.value)}
-      />
-      <button className="btn" onClick={nat}>
-        Execute
-      </button>
-
+      <input id="nmb" type="number" value={num} onChange={(e) => setNum(e.target.value)}/>
+      <button className="btn" onClick={nat}>Execute</button>
       <div>
         {numbers.map((number, index) => (
           <h1 key={index}>{number}</h1>
